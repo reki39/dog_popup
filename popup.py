369256b16a86b85254d9fbc6660f105cc804c228
masayoshi_sakino <102124048+sakino1968@users.noreply.github.com>
2024-08-11 17:58:07 +0900

    修正する場所と内容をコメントで記載。コード修正は自分でやってみてね

--- popup.py
+++ popup.py
@@ -1,12 +1,19 @@
 import PySimpleGUI as sg
 
-
+#ここでresultにsg.popup_yes_noの結果を入れている
 result = sg.popup_yes_no("あなたにおすすめの犬種を探します。あなたが好きなのは超小型犬ですか？")
 
+#↑でresultに入っているyes/noを判定している
 if result == "Yes":
+    #ここでsg.popup_yes_noを使っているが結果をresultに入れていないのでresultの内容は4行目で選んだyes/noのまま
     sg.popup_yes_no("性格は穏やかな子が好きですか？")
+    #つまり正解は以下のようになる。その他のsg.popup_yes_noも全て同じ理由で正しく動作していない
+    # result = を頭に入れることでresultに新たに出てきたポップアップのyes/noの結果を入れ直す。
+    # result = sg.popup_yes_no("性格は穏やかな子が好きですか？")
 
+    #ifでresultを判定するけどここのresultは4行目で選んだyes/noのままなので最初に選んだyes/noの結果にしか行かない
     if result == "Yes":
+     #ここのsg.popupはyes_noではないのでただポップアップを出すだけなのでresult= をつける必要はない
      sg.popup("あなたにはマルチーズ、ヨークシャテリアがおすすめです")
 
     if result == "Yes":
